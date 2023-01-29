f0a817f31b6c19cb8f25f6faa94cd86decf6c096
Adnan Hassan <109860817+imadnanhassan@users.noreply.github.com>
2023-01-29 15:44:12 +0600

    variable

---
+++ loop.js
@@ -0,0 +1,11 @@
+
+/*
+what is loop
+   একটি শর্তের উপর ভিত্তি করে বারবার কাজ সম্পাদন করতে জাভাস্ক্রিপ্টে লুপ ব্যবহার করা হয়। Loops are used in JavaScript to perform repeated tasks based on a condition.
+typw of loop
+   1. for loop
+   2. for of loop
+   3. for in loop
+   4. while loop
+   5. do while loop
+*/ 
---
+++ variable.js
@@ -0,0 +1,10 @@
+/*
+what is variable?
+    variable is simply the name of a storage location.
+
+type of variable?
+   variable 2 type
+      1. global
+      2. local or functional
+*/ 
+
